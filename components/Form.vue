df10f13022a5d83e21ff8e1ef0c4a50d0226a15b
Vlady <contact.johngoldco@gmail.com>
2022-09-15 14:20:50 +0200

    Add style to form with bootstrap

--- components/Form.vue
+++ components/Form.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <h1>Test Form</h1>
+  <div class="d-flex flex-column justify-content-center p-20">
+    <h1 class="mt-10 mb-10 text-center h2">Test Form</h1>
     <form>
-      <label for="name">
-        <input id="name" type="text" />
-      </label>
-      <label for="age"><input id="age" type="number" /></label>
-      <label for="date"></label>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
